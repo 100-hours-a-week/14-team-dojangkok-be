63c944edc0602916c603336c87e8b347ddb74ffc
minjin0610 <138571857+minjin0610@users.noreply.github.com>
2026-01-29 15:06:36 +0900

    Revert "fix: 체크리스트 결과 콜백 문제 해결"

--- src/main/java/com/dojangkok/backend/controller/InternalCallbackController.java
+++ src/main/java/com/dojangkok/backend/controller/InternalCallbackController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/internal/callbacks")
+@RequestMapping("/internal/callbacks")
 public class InternalCallbackController {
 
     private final ChecklistTemplateService checklistTemplateService;
