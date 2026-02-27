5517372e69fe2cb88fb0526a3808a1a7cfe21102
minjin0610 <minjin3755@gmail.com>
2026-02-27 18:24:14 +0900

    feat: DB/Redis 페일오버 시 무중단 설정 갱신을 위한 @RefreshScope 도입

--- src/main/java/com/dojangkok/backend/common/config/RedisConfig.java
+++ src/main/java/com/dojangkok/backend/common/config/RedisConfig.java
@@ -1,21 +1,35 @@
 package com.dojangkok.backend.common.config;
 
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import tools.jackson.databind.json.JsonMapper;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 
 @Configuration
 @Profile("!test")
 public class RedisConfig {
 
     @Bean
+    @RefreshScope
+    public RedisConnectionFactory redisConnectionFactory(RedisProperties redisProperties) {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(redisProperties.getHost());
+        config.setPort(redisProperties.getPort());
+        return new LettuceConnectionFactory(config);
+    }
+
+    @Bean
+    @RefreshScope
     public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory connectionFactory) {
         return new StringRedisTemplate(connectionFactory);
     }
-    
+
     @Bean
     public JsonMapper jsonMapper() {
         return JsonMapper.builder()
