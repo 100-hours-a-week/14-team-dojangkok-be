c3dfff432762a2ebb95aa7b52985626a10757978
minjin0610 <minjin3755@gmail.com>
2026-02-27 13:36:55 +0900

    refactor: 매물 게시글 생성 응답 DTO postId만 포함하는 것으로 수정

---
+++ src/main/java/com/dojangkok/backend/dto/propertypost/PropertyPostCreateResponseDto.java
@@ -0,0 +1,13 @@
+package com.dojangkok.backend.dto.propertypost;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class PropertyPostCreateResponseDto {
+
+    @JsonProperty("property_post_id")
+    private Long propertyPostId;
+}
