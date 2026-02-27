b93fc53d3fb5ea49a04dd01182bf0b738495bda5
minjin0610 <minjin3755@gmail.com>
2026-02-27 20:48:36 +0900

    fix: refreshScope 관련 redis 설정 에러 문제 해결

--- src/main/java/com/dojangkok/backend/common/config/DataSourceConfig.java
+++ src/main/java/com/dojangkok/backend/common/config/DataSourceConfig.java
@@ -2,9 +2,11 @@ package com.dojangkok.backend.common.config;
 
 import javax.sql.DataSource;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,8 +20,11 @@ public class DataSourceConfig {
     @Bean
     @Primary
     @RefreshScope
-    public DataSource dataSource(DataSourceProperties properties) {
-        return properties.initializeDataSourceBuilder().build();
+    @ConfigurationProperties("spring.datasource.hikari")
+    public HikariDataSource dataSource(DataSourceProperties properties) {
+        return properties.initializeDataSourceBuilder()
+                .type(HikariDataSource.class)
+                .build();
     }
 
     @EventListener(ApplicationReadyEvent.class)
