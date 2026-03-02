80a6b380ca599da5a468246933faedd35b2dee19
minjin0610 <minjin3755@gmail.com>
2026-03-03 00:43:34 +0900

    feat: 쉬운 계약서 목록 조회 응답 DTO 항목 총 개수 필드 추가

--- src/main/java/com/dojangkok/backend/dto/easycontract/EasyContractListResponseDto.java
+++ src/main/java/com/dojangkok/backend/dto/easycontract/EasyContractListResponseDto.java
@@ -10,6 +10,9 @@ import java.util.List;
 @Builder
 public class EasyContractListResponseDto {
 
+    @JsonProperty("total_count")
+    private long totalCount;
+
     private int limit;
 
     @JsonProperty("has_next")
